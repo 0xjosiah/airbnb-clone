165f3571a38b349e57b9b4c3724e9b330cce759e
josiahwebb <jj.webb520@gmail.com>
2022-05-11 14:42:11 -0600

    adds sold out badge

--- src/App.js
+++ src/App.js
@@ -3,29 +3,21 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
 import data from "./data"
-/*
-Challenge:
-
-- import the array of data from data.js
-- map over the array to create <Card /> components
-- display the array of card components under the navbar
-  (in place of the current <Card /> component)
-
-Note: We haven't styled the group of components yet, so they'll
-still be block elements, stacked vertically. We'll add styling later.
-*/
 
 export default function App() {
   const cards = data.map(i => {
     return  <Card 
+              key={i.id}
               img={`./images/${i.coverImg}`}
               rating={i.stats.rating}
               reviewCount={i.stats.reviewCount}
               location={i.location}
               title={i.title}
               price={i.price}
+              openSpots={i.openSpots}
             /> 
   })
+
   return (
     <div>
       <Navbar />
